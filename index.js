2542eaae19f6f9cfbef117fa43a71ea18787ba7a
Alexander Bartolomey <occloxium@gmail.com>
2020-02-16 19:36:59 +0100

    Refactor index.js

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(morgan('tiny'));
+if (process.env.NODE_ENV === 'production'){
+    app.use(morgan('tiny'));
+} else if (process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
 app.use(bodyparser.json());
 app.use(cors());
 
-require('./routes/api.js')(app, express.Router());
+const router = express.Router();
 
-const port = 3000;
+const v1 = require('./v1/api.js')(app, router);
+const v2 = require('./v2/api.js')(app, router);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
