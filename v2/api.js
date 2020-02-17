25348a2499eee534829828ca2d65fff0a939aede
Alexander Bartolomey <occloxium@gmail.com>
2020-02-17 08:25:15 +0100

    Fix route registering

--- v2/api.js
+++ v2/api.js
@@ -7,131 +7,116 @@ module.exports = function (app, router) {
   const available_routes = [
     {
       route: '/list',
-      description: 'Retrieve the current shopping list',
-      supported_methods: ['GET'],
-      method: router.get,
-      handler: (_, res, __) => {
-        controller.get().then((data) => {
-          res.json(data);
-        }).catch((err) => {
-          console.log(err);
-          res.status(500).send(err.toString());
-        });
+      description: 'Retrieve or reset the current shopping list',
+      supported_methods: ['GET', 'DELETE'],
+      handler: {
+        GET: (_, res, __) => {
+          controller.get().then((data) => {
+            res.json(data);
+          }).catch((err) => {
+            console.log(err);
+            res.status(500).send(err.toString());
+          });
+        },
+        DELETE: (_, res, __) => {
+          controller.reset().then((data) => {
+            res.json(data);
+          }).catch((err) => {
+            console.log(err);
+            res.status(500).send(err.toString());
+          });
+        }
       }
     },{
       route: '/list/item/:id',
-      description: 'Retrieve a specific item from the current list by id',
-      supported_methods: ['GET'],
-      method: router.get,
-      handler: (req, res, _) => {
-        controller.get(req.params.id).then((data) => {
-          res.json(data);
-        }).catch((err) => {
-          console.log(err);
-          res.status(500).send(err.toString());
-        });
+      description: 'Retrieve, modify or delete a specific item from the current list by id',
+      supported_methods: ['GET', 'PUT', 'DELETE'],
+      handler: {
+        GET: (req, res, _) => {
+          controller.get(req.params.id).then((data) => {
+            res.json(data);
+          }).catch((err) => {
+            console.log(err);
+            res.status(500).send(err.toString());
+          });
+        },
+        PUT: (req, res, _) => {
+          controller.rename(req.params.id, req.body.entry).then((data) => {
+            res.json(data);
+          }).catch((err) => {
+            console.log(err);
+            res.status(500).send(err.toString());
+          });
+        },
+        DELETE: (req, res, _) => {
+          controller.delete(req.params.id).then((data) => {
+            res.json(data);
+          }).catch((err) => {
+            console.log(err);
+            res.status(500).send(err.toString());
+          });
+        }
+      }
+    },{
+      route: '/list/item',
+      description: 'Add a new item to the currently active list',
+      supported_methods: ['POST'],
+      handler: {
+        POST: (req, res, next) => {
+          controller.add(req.body.entry).then((data) => {
+            res.json(data);
+          }).catch((err) => {
+            console.log(err);
+            res.status(500).send(err.toString());
+          });
+        },
       }
     },{
       route: '/lists',
-      description: 'Retrieve a set of identifiers for all *available* lists',
+      description: 'Retrieve a set of identifiers for all available lists',
       supported_methods: ['GET'],
-      method: router.get,
-      handler: (_, res, __) => {
-        controller.lists().then(lists => {
-          res.json(lists);
-        }).catch((err) => {
-          console.log(err);
-          res.status(400).json({
-            error: '400',
-            msg: err.toString(),
+      handler: {
+        GET: (_, res, __) => {
+          controller.lists().then(lists => {
+            res.json(lists);
+          }).catch((err) => {
+            console.log(err);
+            res.status(500).send(err.toString());
           });
-        })
+        }
       }
-    }
+    },
   ];
 
-  const register = ({route, method, handler}) => {
-    method(route, handler);
-  };
+  const active_routes = [];
 
-
-  router.get('/list', );
-
-  router.get('/list/:id', );
-
-  router.get('/lists', );
-
-  router.post('/add', (req, res, next) => {
-    controller.add(req.body.entry).then((data) => {
-      res.json({
-        newEntry: data,
-        status: "ok"
-      });
-      next();
-    }).catch((err) => {
-      console.log(err);
-      res.status(400).json({
-        error: '400',
-        msg: err.toString()
-      });
-    })
-  });
-
-  router.post('/commit', (req, res, next) => {
-    controller.commit().then((data) => {
-      res.json({
-        status: "ok",
-        lastState: data
-      });
-    }).catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: '500',
-        msg: err.toString()
-      });
-    })
-  });
-
-  router.post('/reset', (req, res, next) => {
-    controller.reset().then((data) => {
-      res.json({
-        status: "ok",
-        previousState: data
-      });
-    }).catch((err) => {
-      console.log(err);
-      res.status(400).json({
-        error: '400',
-        msg: err.toString()
-      });
-    })
-  });
-
-  router.delete('/delete/:id', (req, res, next) => {
-    controller.delete(req.params.id).then((data) => {
-      res.json({
-        deletedEntry: data
-      });
-    }).catch((err) => {
-      console.log(err);
-      res.status(400).json({
-        error: '400',
-        msg: err.toString()
-      })
+  available_routes.forEach((r) => {
+    r.supported_methods.forEach((m) => {
+      switch(m){
+        case 'GET':
+          router.get(r.route, r.handler[m]);
+          break;
+        case 'POST':
+          router.post(r.route, r.handler[m]);
+          break;
+        case 'PUT':
+          router.put(r.route, r.handler[m]);
+          break;
+        case 'DELETE':
+          router.delete(r.route, r.handler[m]);
+          break;
+        default: break;
+      }
+    });
+    
+    active_routes.push({
+      route: process.env.HOST + '/api/v2' + r.route,
+      description: r.description,
+      supported_methods: r.supported_methods,
     });
   });
 
-  router.post('/rename/:id', (req, res, next) => {
-    controller.rename(req.params.id, req.body.entry).then((data) => {
-      res.json({
-        changedEntry: data
-      });
-    }).catch((err) => {
-      console.log(err);
-      res.status(400).json({
-        error: '400',
-        msg: err.toString()
-      })
-    });
-  });
+  router.get('/', (_, res, __) => {
+    res.json(active_routes);
+  })
 }
